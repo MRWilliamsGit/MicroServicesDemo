31c06997013d7ea8f9719c01ff2e7db0de6fa69f
Maria Williams <mariawilliams.professional@gmail.com>
2022-02-07 19:26:18 +0000

    updates

--- heythere.py
+++ heythere.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello There Friends!"}
+    return {"message": "Hello There Friend!"}
 
 @app.get("/add/{num1}/{num2}")
 async def add(num1: int, num2: int):
@@ -13,6 +13,10 @@ async def add(num1: int, num2: int):
 
     total = num1 + num2
     return {"total": total}
+
+@app.get("/hey/{nm}")
+async def hey(nm: str):
+    return {"message": "You have a nice name, "&nm&"!"}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
